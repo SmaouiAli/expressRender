1572e0f8a337de0562e666248dbe43bab585c387
SmaouiAli <smaouiali22@gmail.com>
2022-12-07 12:11:59 +0100

    added post handler

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
-
+const users = []
 app.get('/',(req,res)=>{
     res.json("hello world")
 })
-
+app.post('/',(req,res)=>{
+    users.push(req.body.user)
+    res.json(req.body.user)
+})
+app.get("/user",(req,res)=>{
+    res.json(users)
+})
 
 app.listen(PORT,()=>{
     console.log("server running");
